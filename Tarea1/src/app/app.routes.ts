aaf28d23e2bcaf5f4595bfc734523580a858578d
pablo moreno <pablommoreno5@gmail.com>
2025-04-18 19:48:24 -0300

    modificacion de tarea1

--- Tarea1/src/app/app.routes.ts
+++ Tarea1/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { BienvenidaComponent } from './pages/bienvenida/bienvenida.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+
+    path: "bienvenida",
+    component: BienvenidaComponent,
+    
+},{
+    path: "Login",
+    component: LoginComponent,
+},{
+    path: "registro",
+    component: RegistroComponent,
+},{
+    path: "**",
+    component: ErrorComponent,
+}];
